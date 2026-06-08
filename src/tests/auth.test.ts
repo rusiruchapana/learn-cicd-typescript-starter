1487b4db4f1af1bb4277efe948bf3322a9197e0f
Rusiru chapana <rusiru.chapana@atlato.com>
2026-06-08 11:28:58 +0530

    prettier write

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -24,16 +24,11 @@ describe("getAPIKey", () => {
     expect(result).toBeNull();
   });
 
-
-
-
-
-
   test("should return the API key if the authorization header is valid", () => {
     const headers = {
       authorization: "ApiKey valid-api-key",
     };
     const result = getAPIKey(headers);
-                                expect(result).toBe("valid-api-key");
-                            });
+    expect(result).toBe("valid-api-key");
+  });
 });
